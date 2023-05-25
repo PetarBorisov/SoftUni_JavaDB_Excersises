c255e0fea1312ae21e731075cb5a4bf873da3451
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-05-25 22:49:39 +0300

    Update Home Work 4.sql

--- Home Work 4.sql
+++ Home Work 4.sql
@@ -65,3 +65,85 @@ FROM
     wizzard_deposits
 GROUP BY deposit_group , magic_wand_creator
 ORDER BY magic_wand_creator , deposit_group;
+ 
+-- 9
+SELECT 
+    (CASE
+        WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
+        WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
+        WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
+        WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
+        WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
+        WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
+        WHEN `age` >= 61 THEN '[61+]'
+    END) AS 'age_group',
+    COUNT(`first_name`) AS 'wizzard_count'
+FROM
+    `wizzard_deposits`
+GROUP BY `age_group`
+ORDER BY `wizzard_count` ASC;
+
+-- 10
+SELECT 
+    LEFT(`first_name`, 1) AS 'first_letter'
+FROM
+    `wizzard_deposits`
+WHERE
+    `deposit_group` = 'Troll Chest'
+GROUP BY `first_letter`
+ORDER BY `first_letter`;
+
+-- 11
+SELECT 
+    `deposit_group`,
+    `is_deposit_expired`,
+    AVG(`deposit_interest`) AS 'average_interest'
+FROM
+    `wizzard_deposits`
+WHERE
+    `deposit_start_date` > '1985-01-01'
+GROUP BY `deposit_group` , `is_deposit_expired`
+ORDER BY `deposit_group` DESC , `is_deposit_expired` ASC;
+
+-- 12
+SELECT 
+    `department_id`, min(`salary`) as 'minimum_salary'
+FROM
+    employees
+WHERE
+    `hire_date` > '2000-01-01'
+GROUP BY `department_id`
+HAVING `department_id` IN ( 2, 5, 7)
+ORDER BY `department_id`;
+
+-- 13
+CREATE TABLE `salary_more_than_30000` AS SELECT * FROM
+    `employees`
+WHERE
+    `salary` > 30000;
+
+DELETE FROM `salary_more_than_30000` 
+WHERE
+    `manager_id` = 42;
+
+UPDATE `salary_more_than_30000` 
+SET 
+    `salary` = `salary` + 5000
+WHERE
+    `department_id` = 1;
+
+SELECT 
+    `department_id`, AVG(`salary`) AS 'avg_salary'
+FROM
+    `salary_more_than_30000`
+GROUP BY `department_id`
+ORDER BY `department_id` ASC;
+
+-- 14
+SELECT 
+    `department_id`, MAX(`salary`) AS 'max_salary'
+FROM
+    `employees`
+GROUP BY `department_id`
+HAVING `max_salary` NOT BETWEEN 30000 AND 70000
+ORDER BY `department_id` ASC;
