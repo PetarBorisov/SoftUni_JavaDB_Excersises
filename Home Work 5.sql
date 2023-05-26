59822d4cab3e4567e1d15993a14fa175b2e8c454
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-05-27 00:56:36 +0300

    Update Home Work 5.sql

--- Home Work 5.sql
+++ Home Work 5.sql
@@ -192,3 +192,71 @@ ALTER TABLE orders
 ADD CONSTRAINT fk_orders_customers
 FOREIGN KEY (customer_id)
 REFERENCES customers(customer_id);
+
+-- 6
+CREATE DATABASE university;
+
+CREATE TABLE subjects (
+	subject_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    subject_name VARCHAR(50)
+);
+
+CREATE TABLE majors (
+	major_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) 
+);
+
+CREATE TABLE payments (
+	payment_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    payment_date DATE,
+    payment_amount DECIMAL(8, 2),
+    student_id INT(11)
+);
+
+CREATE TABLE students (
+	student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_number VARCHAR(12) UNIQUE,
+    student_name VARCHAR(50),
+    major_id INT
+);
+
+CREATE TABLE agenda (
+	student_id INT,
+    subject_id INT,
+    CONSTRAINT pk
+    PRIMARY KEY (student_id, subject_id)
+);
+
+
+ALTER TABLE payments
+ADD CONSTRAINT fk_payments_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id);
+
+
+ALTER TABLE students
+ADD CONSTRAINT fk_students_majors
+FOREIGN KEY (major_id)
+REFERENCES majors(major_id);
+
+
+ALTER TABLE agenda
+ADD CONSTRAINT fk_agenda_subjects
+FOREIGN KEY (subject_id)
+REFERENCES subjects(subject_id);
+
+ALTER TABLE agenda
+ADD CONSTRAINT fk_agenda_students
+FOREIGN KEY (student_id)
+REFERENCES students(student_id);
+
+-- 9
+SELECT 
+    m.`mountain_range`, p.`peak_name`, p.`elevation`
+FROM
+    `mountains` AS m
+        JOIN
+    `peaks` AS p ON p.`mountain_id` = m.`id`
+WHERE
+    m.`mountain_range` = 'Rila'
+ORDER BY p.`elevation` DESC;
