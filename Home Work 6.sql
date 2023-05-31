a9426a7c64c32d91e63e9b699b0f7e70dfe30002
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-06-01 00:27:06 +0300

    Update Home Work 6.sql

--- Home Work 6.sql
+++ Home Work 6.sql
@@ -201,3 +201,33 @@ HAVING currency_usage > 1
     ORDER BY count_of_currencies DESC
     LIMIT 1)
 ORDER BY c.continent_code , c.currency_code;
+
+-- 16
+SELECT COUNT(*) as country_count FROM (SELECT 
+        mc.country_code AS 'mc_country_code'
+    FROM
+        `mountains_countries` AS mc
+    GROUP BY mc.country_code) AS d
+        RIGHT JOIN
+    `countries` AS c ON c.country_code = d.mc_country_code
+WHERE
+    d.mc_country_code IS NULL;
+
+-- 17
+SELECT 
+    c.country_name,
+    MAX(p.elevation) AS higest_peak_elevation,
+    MAX(r.length) AS longest_river_lenght
+FROM
+    countries AS c
+        LEFT JOIN
+    mountains_countries AS mc ON c.country_code = mc.country_code
+        LEFT JOIN
+    peaks AS p ON mc.mountain_id = p.mountain_id
+        LEFT JOIN
+    countries_rivers AS cr ON c.country_code = cr.country_code
+        LEFT JOIN
+    rivers AS r ON cr.river_id = r.id
+GROUP BY c.country_name
+ORDER BY higest_peak_elevation DESC , longest_river_lenght DESC , c.country_name
+LIMIT 5;
