94b7f6a894294d3b20192d35d4fd9ebfc1bebf4c
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-06-08 23:51:51 +0300

    Update 10 april 2022 exam.sql

--- 10 april 2022 exam.sql
+++ 10 april 2022 exam.sql
@@ -123,9 +123,35 @@ ORDER BY budget DESC;
 
 
 -- 10
+DELIMITER $$
+CREATE FUNCTION udf_actor_history_movies_count(full_name VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+DECLARE movies_count INT;
+SET movies_count := 
+(SELECT COUNT(g.name) as movies FROM actors AS a
+ JOIN movies_actors ma on a.id = ma.actor_id
+                 JOIN genres_movies gm on ma.movie_id = gm.movie_id
+                 JOIN genres g on g.id = gm.genre_id
+        WHERE CONCAT(a.first_name, ' ', a.last_name) = full_name AND g.name = 'History'
+        GROUP BY  g.name);
+    RETURN movies_count;
+end $$
+DELIMITER ;
 
-
-
+-- 11
+DELIMITER $$
+CREATE PROCEDURE udp_award_movie(`movie_title`VARCHAR(50))
+BEGIN
+    UPDATE actors a
+        JOIN movies_actors ma on a.id = ma.actor_id
+        JOIN movies m on m.id = ma.movie_id
+    SET  a.awards = a.awards + 1
+    WHERE m.title = movie_title;
+END $$
+ DELIMITER ;
+CALL udp_award_movie('Tea For Two');
 
 
 
