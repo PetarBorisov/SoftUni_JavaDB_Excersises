b06045db2c0a03f78d36b4b2015f49a4dbbb2ed1
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-06-29 17:16:37 +0300

    Create Main.java

---
+++ SpringData/HibernateFirstExersize/HibernateFirstExersize/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import entity.Product;
+import entity.Sale;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManager entityManager = Persistence
+                .createEntityManagerFactory("HibernateF")
+                .createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+       Sale sale = new Sale();
+        sale.setDateTime(LocalDateTime.now());
+
+        Product product = new Product();
+        product.setName("TestProduct");
+        product.setPrice(BigDecimal.TEN);
+        product.setQuantity(5);
+        product.getSales().add(sale);
+        sale.setProduct(product);
+
+
+        entityManager.persist(product);
+
+       // Product found = entityManager.find(Product.class,1L);
+     //   entityManager.remove(found);
+
+
+        entityManager.getTransaction().commit();
+    }
+}
