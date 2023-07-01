68de91ad711635a694a2a83b94d16b5fc25b3a91
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-07-01 12:08:33 +0300

    Create BankAccount.java

---
+++ SpringData/HibernateFirstExersize/src/main/java/entity/BankAccount.java
@@ -0,0 +1,32 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bank_accounts")
+public class BankAccount extends BillingDetail {
+
+    private String name;
+    private String swift;
+
+    public BankAccount() {
+    }
+@Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+@Column
+    public String getSwift() {
+        return swift;
+    }
+
+    public void setSwift(String swift) {
+        this.swift = swift;
+    }
+}
