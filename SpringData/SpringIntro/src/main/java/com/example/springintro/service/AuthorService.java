7bbd93a275edf60a7e06a3a869eb75d1b681f246
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-07-04 17:29:37 +0300

    Update AuthorService.java

--- SpringData/SpringIntro/src/main/java/com/example/springintro/service/AuthorService.java
+++ SpringData/SpringIntro/src/main/java/com/example/springintro/service/AuthorService.java
@@ -3,9 +3,12 @@ package com.example.springintro.service;
 import com.example.springintro.Entity.Author;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface AuthorService {
     void seedAuthors() throws IOException;
 
     Author getRandomAuthor();
+
+    List<String> getAllAuthorsOrderByCountOfTheirBooks();
 }
