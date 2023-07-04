500ffe3594b4ac3e3b2b8466377f8fd87a7fa803
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-07-04 17:29:40 +0300

    Update BookService.java

--- SpringData/SpringIntro/src/main/java/com/example/springintro/service/BookService.java
+++ SpringData/SpringIntro/src/main/java/com/example/springintro/service/BookService.java
@@ -1,7 +1,17 @@
 package com.example.springintro.service;
 
+import com.example.springintro.Entity.Book;
+
 import java.io.IOException;
+import java.util.List;
 
 public interface BookService {
     void seedBooks() throws IOException;
+
+    List<Book> findAllBooksAfterYear(int year);
+
+    List<String> findAllAuthorsWithBooksWithReleasesDataBeforeYear(int year);
+
+    List<String> findAllBooksByAuthorFirstAndLastNameOrderByReleaseDate(String firstName,String lastName);
+
 }
