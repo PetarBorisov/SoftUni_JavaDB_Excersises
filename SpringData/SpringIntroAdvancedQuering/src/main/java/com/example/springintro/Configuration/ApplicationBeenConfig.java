ec5f42bf5d0a91ca30f8ba3938bd486ae906fef3
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-07-05 16:31:47 +0300

    Create ApplicationBeenConfig.java

---
+++ SpringData/SpringIntroAdvancedQuering/src/main/java/com/example/springintro/Configuration/ApplicationBeenConfig.java
@@ -0,0 +1,16 @@
+package com.example.springintro.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@Configuration
+public class ApplicationBeenConfig {
+
+    @Bean
+    public BufferedReader bufferedReader(){
+        return new BufferedReader(new InputStreamReader(System.in));
+    }
+}
