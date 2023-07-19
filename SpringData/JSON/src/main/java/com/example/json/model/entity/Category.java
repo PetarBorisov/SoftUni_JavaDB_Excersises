0c8aa8db5cdae258aa538b96b05ee367fb68f64d
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-07-20 00:25:33 +0300

    Create Category.java

---
+++ SpringData/JSON/src/main/java/com/example/json/model/entity/Category.java
@@ -0,0 +1,43 @@
+package com.example.json.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {
+
+    private String name;
+
+
+
+    public Category() {
+    }
+     @Column(length = 15,nullable = false,unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
