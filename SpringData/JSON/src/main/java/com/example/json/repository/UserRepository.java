40151a46e9c58f1860f85ab47cbe02c3591e8c9b
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-07-21 00:42:00 +0300

    Create UserRepository.java

---
+++ SpringData/JSON/src/main/java/com/example/json/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.json.repository;
+
+import com.example.json.model.entity.User;
+import org.hibernate.annotations.Where;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+@Query("SELECT u FROM User u " +
+      "WHERE (SELECT COUNT(p) FROM Product p WHERE p.seller.id = u.id ) > 0 " +
+      "ORDER BY  u.lastName, u.firstName")
+    List<User> findAllUsersWithMoreThanOneSoldProductOrderByLastNameThenFirstName();
+}
