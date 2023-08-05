e34ea76e388bae7edb056c02e052688ed57d8238
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-08-05 06:44:25 +0300

    Create Messages.java

---
+++ SpringData/SoftWeather Forecast/src/main/java/softuni/exam/models/Messages.java
@@ -0,0 +1,13 @@
+package softuni.exam.models;
+
+public enum Messages {
+    ;
+    public static final String INVALID_COUNTRY = "Invalid country%n";
+    public static final String VALID_COUNTRY_FORMAT = "Successfully imported country %s - %s%n";
+    public static final String VALID_CITY_FORMAT = "Successfully imported city %s - %d";
+    public static final String INVALID_CITY = "Successfully imported city";
+    public static final String Valid_Forecast = "Successfully imported forecast %s - %.2f";
+    public static final String Invalid_Forecast = "Invalid forecast";
+
+
+}
