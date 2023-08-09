d59834305625951df3039883d37ea76ed0b2a5f3
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-08-09 21:18:10 +0300

    Create CityService.java

---
+++ SpringData/SoftWeather Forecast/src/main/java/softuni/exam/service/CityService.java
@@ -0,0 +1,19 @@
+package softuni.exam.service;
+
+import softuni.exam.models.entity.City;
+
+import java.io.IOException;
+import java.util.Optional;
+
+
+public interface CityService {
+
+    boolean areImported();
+
+    String readCitiesFileContent() throws IOException;
+	
+	String importCities() throws IOException;
+
+    City findCityById(Long city);
+
+}
