44c097c286018e61d1cc34d99e08aefee18dd98c
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-09-15 15:39:38 +0300

    Create TasksSeedRootDto.java

---
+++ SpringData/CarService/src/main/java/softuni/exam/models/dto/TasksSeedRootDto.java
@@ -0,0 +1,24 @@
+package softuni.exam.models.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "tasks")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TasksSeedRootDto {
+
+
+    @XmlElement(name = "task")
+    List<TaskSeedDto> tasks;
+
+    public List<TaskSeedDto> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskSeedDto> tasks) {
+        this.tasks = tasks;
+    }
+}
