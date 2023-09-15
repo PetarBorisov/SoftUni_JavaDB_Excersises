e7b171a1ee5fc9894e4b140e7e2ae086a88f2207
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-09-16 00:11:44 +0300

    Create MechanicsRepository.java

---
+++ SpringData/CarService/src/main/java/softuni/exam/repository/MechanicsRepository.java
@@ -0,0 +1,19 @@
+package softuni.exam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Mechanic;
+
+import java.util.Optional;
+
+@Repository
+public interface MechanicsRepository extends JpaRepository<Mechanic, Long> {
+
+
+    Optional<Object> findFirstByEmail(String email);
+
+    Optional<Mechanic> findFirstByFirstName(String fistName);
+
+    Optional<Mechanic> findAllByFirstName(String firstName);
+
+}
